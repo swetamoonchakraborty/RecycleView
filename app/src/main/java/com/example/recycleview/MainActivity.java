180343304503ede3e88eef1b14d66ab79626d2d0
DronaSoftwares <dronasoftwares@gmail.com>
2020-07-02 16:34:11 +0530

    Code refactored

--- app/src/main/java/com/example/recycleview/MainActivity.java
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.recycleview;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.view.menu.ListMenuItemView;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -6,18 +7,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.LauncherActivity;
-import	android.app.LauncherActivity.ListItem;
+import android.app.LauncherActivity.ListItem;
 import android.os.Bundle;
-import	android.app.LauncherActivity.ListItem;
+import android.app.LauncherActivity.ListItem;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
-
 
 
     @Override
@@ -26,26 +25,24 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
 
-            ArrayList<Itemlist> items = new ArrayList<Itemlist>();
+        ArrayList<Itemlist> items = new ArrayList<Itemlist>();
 
-            for (int i = 1; i <= 10; i++) {
-                items.add(new Itemlist("heading " + i + 1, "Test"));
+        for (int i = 1; i <= 10; i++) {
+            items.add(new Itemlist("heading " + i + 1, "Test"));
 
-            }
+        }
 
-        recyclerView=(RecyclerView) findViewById(R.id.recyclerView);
-        adapter=new Itemadapter(items,this);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        adapter = new Itemadapter(items, this);
         recyclerView.setAdapter(adapter);
 
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 
-
-
     }
 
-    }
+}
 
 
 
